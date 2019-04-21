046a3e546c73c84f815f48467bfa74967d083d3e
Skora120 <radoslaw.skorzewski@gmail.com>
2019-04-21 23:35:29 +0200

    some front end for sending submissions

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,7 @@ require('@fortawesome/fontawesome-free');
 require('datatables.net-bs4');
 
 require('./sb-admin-2.js');
+require('./dropzone.js');
 
 window.Vue = require('vue');
 
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/test', function(){
 	return view('test');
 });
+
+Route::get('/test_send_task', function(){
+	return view('test_send_task');
+});
