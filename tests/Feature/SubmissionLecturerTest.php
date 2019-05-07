a6a0be04c5c1a3a978e81b758068fd1a689bbc54
Skora120 <radoslaw.skorzewski@gmail.com>
2019-05-07 22:54:24 +0200

    submissions tests refactor

--- tests/Feature/SubmissionLecturerTest.php
+++ tests/Feature/SubmissionLecturerTest.php
@@ -9,50 +9,46 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class SubmissionLecturerTest extends TestCase
 {
     use RefreshDatabase;
-    
+    protected $submission;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->submission = factory('App\Submission')->create();
+    }
+
     /** @test */
     public function an_lecturer_may_view_submission()
     {
-        $submission = factory('App\Submission')->create();
-
-        $lecturer = $submission->task->group->subject->user;
-        $this->be($lecturer);
-
-        $this->get($submission->path())->assertSee($submission->s_comment);
+        $this->be($this->submission->task->group->subject->user);
+        $this->get($this->submission->path())->assertSee($this->submission->s_comment);
     }
 
     /** @test */
     public function an_unauthorized_user_cannot_see_submission()
     {
         $this->be(factory('App\User')->create());
-
-        $submission = factory('App\Submission')->create();
-
-        $this->get($submission->path())->assertDontSee($submission->s_comment);
+        $this->get($this->submission->path())->assertDontSee($this->submission->s_comment);
     }
 
     /** @test */
     public function an_lecturer_may_mark_and_leave_response_on_submission()
     {
-        $submission = factory('App\Submission')->create();
-
-        $this->be($submission->task->group->subject->user);
+        $this->be($this->submission->task->group->subject->user);
 
         $submissionData = factory('App\Submission')->make()->toArray();
 
-        $this->patch($submission->path(), $submissionData)->assertStatus(302);
-        $this->get($submission->path())->assertSee($submissionData['r_comment']);
+        $this->patch($this->submission->path(), $submissionData)->assertStatus(302);
+        $this->get($this->submission->path())->assertSee($submissionData['r_comment']);
     }
 
     /** @test */
     public function an_unauthorized_user_cannot_update_submission()
     {
-        $submission = factory('App\Submission')->create();
-
         $this->be(factory('App\User')->create());
-
         $submissionData = factory('App\Submission')->make()->toArray();
 
-        $this->patch($submission->path(), $submissionData)->assertStatus(403);
+        $this->patch($this->submission->path(), $submissionData)->assertStatus(403);
     }
 }
