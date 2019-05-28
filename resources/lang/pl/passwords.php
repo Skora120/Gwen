522a9e9e78eb161a24faf4d9ee8181f8f8b03726
Skora120 <radoslaw.skorzewski@gmail.com>
2019-05-28 21:04:49 +0200

    messages validation translation to pl

---
+++ resources/lang/pl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Hasło musi zawierać conajmniej 8 znaków i podane hasła muszą się zgadzać.',
+    'reset' => 'Twoje hasło zostało zresetowane!',
+    'sent' => 'Został wysłany email z linkiem do zresetowania hasła!',
+    'token' => 'Token resetujący hasło jest nieprawidłowy.',
+    'user' => "Nie możemy znaleźć użytkownika z takiem adresem email.",
+
+];
