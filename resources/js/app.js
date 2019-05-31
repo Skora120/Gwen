5111f080d5756c8f8bb217ca54fa6f420aca938c
Skora120 <radoslaw.skorzewski@gmail.com>
2019-05-31 13:41:58 +0200

    Js for sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,12 +7,7 @@
 
 require('./bootstrap');
 
-require('chart.js');
 require('@fortawesome/fontawesome-free');
-// require('datatables.net-bs4');
-
-require('./sb-admin-2.js');
-// require('./dropzone.js');
 
 window.Vue = require('vue');
 
